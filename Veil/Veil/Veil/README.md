1ef0e6db327ebbf2dce483cd300c1e9d28e83373
orseti <hello@orseti.com>
2023-01-05 18:44:51 +0000

    various changes, weathe4r and maps setup

--- Veil/Veil/Veil/README.md
+++ Veil/Veil/Veil/README.md
@@ -13,10 +13,16 @@ It manages all computing interfaces and capabilities of the entire van, and cent
 - Weather
 - Photo and Video Library, sorted, geotagged onto map.
 - Detailed Stock, with location inside Icarus, and steps to get to it.
-- Voice Commands.
 
 
 ### Unconfirmed Features
 
 - CCTV
+- Blogging/Content Creation Platform
+- Finicial Management.
+
+## Voice Commands
+
+This will be likely using mycroft (https://github.com/MycroftAI/mycroft-core#using-mycroft) and cannot be built into a browser,
+so we will have to seperately intergrate this. Will need a linux machine, and then build up an enviroment for it to run in.
 
---
+++ Veil/Veil/Veil/wwwroot/scripts/Maps.js
@@ -0,0 +1,41 @@
+﻿async function InitMap() {
+
+    var location = await getCoords();
+    setTextBox('lat', location.lat)
+    setTextBox('long', location.long)
+
+
+
+
+    var map = L.map('map', {
+        center: [location.lat, location.long],
+        zoom: 12
+    });
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+}
+
+
+const getCoords = async () => {
+        const pos = await new Promise((resolve, reject) => {
+          navigator.geolocation.getCurrentPosition(resolve, reject);
+        });
+    
+        return {
+          long: pos.coords.longitude,
+          lat: pos.coords.latitude,
+        };
+};
+
+
+function setTextBox(type, value) {
+    if (type === 'lat') {
+
+        let latTextbox = document.getElementById("latitudeText");
+        latTextbox.value = value;
+    } else if (type === 'long') {
+        let longTextbox = document.getElementById("longitudeText");
+        longTextbox.value = value;
+    }
+}
