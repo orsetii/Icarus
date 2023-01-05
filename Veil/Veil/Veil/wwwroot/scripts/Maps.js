c4cf4dc2b807ca6665288e66c8af13caaac19dcd
orseti <hello@orseti.com>
2023-01-05 18:47:28 +0000

    remove attr

--- Veil/Veil/Veil/wwwroot/scripts/Maps.js
+++ Veil/Veil/Veil/wwwroot/scripts/Maps.js
@@ -12,7 +12,7 @@
         zoom: 12
     });
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    attribution: ''
 }).addTo(map);
 }
 
